36d7e6b636994a15df6c213bb2a554a66bfbe550
Luis Paz <lpaz8806@gmail.com>
2023-03-23 10:53:33 +0100

    FormatListService extracted and set in constructor

--- src/pages/search/SearchViewModel.ts
+++ src/pages/search/SearchViewModel.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'inversify';
 import { action, makeObservable, observable } from 'mobx';
 import type ApiServiceType from '../../service/ApiService';
+import type FormatListService from "../../service/FormatList";
 
 export type SearchType = {
   searchValue: string;
@@ -17,7 +18,7 @@ export default class SearchViewModel implements SearchType {
   public searchResults = [{}];
 
   constructor(
-      @inject('FORMAT_LIST_SERVICE') private formatListService: any,
+      @inject('FORMAT_LIST_SERVICE') private formatListService: FormatListService,
       @inject('API_SERVICE') private apiService: ApiServiceType,
   ) {
     makeObservable(this, {
--- src/service/FormatList.ts
+++ src/service/FormatList.ts
@@ -1,7 +1,11 @@
-import { injectable } from "inversify";
+import {injectable} from "inversify";
+
+export interface FormatListService {
+  formatList: (list: Array<any>) => Array<any>;
+}
 
 @injectable()
-export default class FormatList {
+export default class FormatList implements FormatListService {
   public formatList = (list: Array<any>): Array<any> => {
     return list.map((item: any) => {
       return {
