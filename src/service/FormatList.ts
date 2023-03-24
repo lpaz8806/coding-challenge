3fd56346b5eba5abad72e9317e9d04d6ee28b489
Luis Paz <lpaz8806@gmail.com>
2023-03-24 01:42:04 +0100

    FormatList types set

--- src/service/FormatList.ts
+++ src/service/FormatList.ts
@@ -1,19 +1,21 @@
 import {injectable} from "inversify";
+import {Product} from "../types/Product";
+import {ProductListItem} from "../types/ProductListItem";
 
-export interface FormatListService {
-  formatList: (list: Array<any>) => Array<any>;
+export interface FormatListService<TSource = any, TDest = any> {
+  formatList: (list: Array<TSource>) => Array<TDest>;
 }
 
 @injectable()
-export default class FormatList implements FormatListService {
-  public formatList = (list: Array<any>): Array<any> => {
-    return list.map((item: any) => {
+export default class FormatList implements FormatListService<Product, ProductListItem> {
+  public formatList = (list: Array<Product>): Array<ProductListItem> => {
+    return list.map((item: Product) => {
       return {
         id: item.id,
         title: item.title,
         description: item.description,
         price: item.price,
-      };
+      }
     });
   }
 }
