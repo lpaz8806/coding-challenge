62f18dbd49f05b4dc98c9856b377e17a6c99c22a
Luis Paz <lpaz8806@gmail.com>
2023-03-24 01:40:12 +0100

    Create SearchBoxView.tsx

---
+++ src/pages/search/SearchBoxView.tsx
@@ -0,0 +1,37 @@
+import {Button, Stack, TextField} from "@mui/material";
+import React, {ReactElement} from "react";
+
+export type SearchBoxViewProps = {
+    onSearchTermChange: (term: string) => void | null;
+    onSearchRequested: () => void | null;
+}
+export function SearchBoxView(props: SearchBoxViewProps) : ReactElement {
+
+    return (
+    <Stack direction="row">
+        <TextField
+            id="outlined-basic"
+            label="Search for an article"
+            variant="outlined"
+            onChange={(e) => props?.onSearchTermChange(e.target.value)}
+        />
+        <Button
+            style={ButtonStyle}
+            sx={{ marginLeft: 2 }}
+            variant="contained"
+            onClick={(e) => props?.onSearchRequested()}
+        >
+            Search
+        </Button>
+    </Stack>
+    );
+}
+
+const ButtonStyle = {
+    backgroundColor: '#3f51b5',
+    color: '#fff',
+    '&:hover': {
+        backgroundColor: '#3f51b5',
+        color: '#fff',
+    },
+};
