3c1d74fdf413bc874f3c09b4a4bd00df6800d1d6
Luis Paz <lpaz8806@gmail.com>
2023-03-24 01:41:41 +0100

    ApiService types set

--- src/service/ApiService.ts
+++ src/service/ApiService.ts
@@ -1,14 +1,15 @@
 import { injectable } from "inversify";
 import mockData from "./mockData.json";
 import {ApiResponseType} from "../types/ApiResponseType";
+import {Product} from "../types/Product";
 
-export interface ApiServiceType {
-  get: () => Promise<ApiResponseType>;
+export interface ApiServiceType<T = any> {
+  get: () => Promise<ApiResponseType<T>>;
 }
 
 @injectable()
-export default class ApiService implements ApiServiceType {
-  public get = async (): Promise<ApiResponseType> => {
+export default class ApiService implements ApiServiceType<Array<Product>> {
+  public get = async (): Promise<ApiResponseType<Array<Product>>> => {
     return mockData;
   }
 }
