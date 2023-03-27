be454f09aec8409bd1a0e80ce1e38f5caa0a51f4
Luis Paz <lpaz8806@gmail.com>
2023-03-27 11:48:17 +0200

    Update Search.test.tsx

--- src/pages/search/tests/Search.test.tsx
+++ src/pages/search/tests/Search.test.tsx
@@ -1,9 +1,24 @@
 import { render } from "@testing-library/react";
-import Search from "../Search";
+import {Search} from "../Search";
+import SearchViewModelType from "../../../types/SearchViewModelType";
+import {ProductListItem} from "../../../types/ProductListItem";
 
 describe('SearchPage', () => {
   it('should render', () => {
-    const { container } = render(<Search />);
+    const viewModel = initViewModel();
+    const { container } = render(<Search viewModel={viewModel} />);
     expect(container).toMatchSnapshot();
   });
 });
+
+function initViewModel(args: Partial<SearchViewModelType> = { }): SearchViewModelType {
+  const defaultViewModel: SearchViewModelType = {
+    search: async () => { },
+    searchResults: [],
+    searchValue: "",
+    selectedProduct: null,
+    setSearchValue: (value: string) => { },
+    setSelectedProduct(selectedProduct: ProductListItem | null): void {}
+  };
+  return Object.assign({}, defaultViewModel, args);
+}
